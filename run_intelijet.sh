d3a7f4e35f397d3b6049b51aa5dee8e7b67c6751
Vo Hoai Danh <jacondv@gmail.com>
2025-08-20 15:58:24 +0700

    tiny update

--- run_intelijet.sh
+++ run_intelijet.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo ">>> Setup container ..."
 cd /root/intelijet_v2/intelijet_v2_ws || exit 1
+export DISABLE_ROS1_EOL_WARNINGS=1
 source devel/setup.bash
 roslaunch pps pps.launch
 exec bash
