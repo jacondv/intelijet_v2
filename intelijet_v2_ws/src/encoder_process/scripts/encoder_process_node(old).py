b23c14eee60b1c63dfa4c256552c43ddc5e1e854
Vo Hoai Danh <jacondv@gmail.com>
2026-03-24 14:00:12 +0700

    Update code for eRob Encoder

--- intelijet_v2_ws/src/encoder_process/scripts/encoder_process_node(old).py
+++ intelijet_v2_ws/src/encoder_process/scripts/encoder_process_node(old).py
@@ -1,53 +1,42 @@
 #!/usr/bin/env python3
-
-# 2026-03-24 Change to eRob Motor, now we can read the Housing angle value from the PLC
-
-
 import rospy
 from sensor_msgs.msg import JointState
 from can_msgs.msg import Frame
-from std_msgs.msg import Int32, Float32
-
+from std_msgs.msg import Int32
 from encoder_process import encoder_utils as solver
-from encoder_process.base_encoder import BaseEncoder, EncoderEROB
-
 from shared.config_loader import CONFIG
 cfg = CONFIG
 ENCODER_DATA_TOPIC =  cfg.ENCODER01_DATA
-
 class EncoderProcessNode:
-    def __init__(self, encoder: BaseEncoder):
+    def __init__(self):
         rospy.init_node("encoder_process_node")
-        self.encoder = encoder
 
         # ROS Pub/Sub
         self.pub_joint_states = rospy.Publisher("/joint_states", JointState, queue_size=10)
         rospy.Subscriber(cfg.ENCODER01_CAN_MSG, Frame, self.can_callback)
-        self.pub_encoder_data = rospy.Publisher(ENCODER_DATA_TOPIC, Float32, queue_size=10)
+        self.pub_encoder_data = rospy.Publisher(ENCODER_DATA_TOPIC, Int32, queue_size=10)
 
     def can_callback(self, msg: Frame):
-        data_bytes = bytes(msg.data)
+        draw_wire_length = solver.convert_draw_wire_length(msg.data)
+        angle = solver.length_to_angle_polynomial(draw_wire_length)
+ 
+        #rospy.logwarn(f"[EncoderProcessNode] Length {draw_wire_length} to angle {angle} rad, {angle*180/3.14} deg.")
 
-        # decode theo encoder hiện tại
-        angle = self.encoder.decode_angle(data_bytes)
-        raw_val = self.encoder.decode_raw(data_bytes)
-
-        # publish JointState
+        # Publish JointState
         joint_msg = JointState()
         joint_msg.header.frame_id = "base_link"
         joint_msg.header.stamp = rospy.Time.now()
         # Gắn encoder vào trục quay của robot, khai báo trong scanner_housing.urdf
-        joint_msg.name = ["scanner_deploy_retract"]
+        joint_msg.name = ["scanner_deploy_retract"] 
         joint_msg.position = [angle]
         self.pub_joint_states.publish(joint_msg)
 
-        # publish raw encoder
+        encoder_value_raw = solver.convert_can_to_encoder_value(msg.data)
         msg_int = Int32()
-        msg_int.data = int(raw_val)
+        msg_int.data = int(encoder_value_raw)
         self.pub_encoder_data.publish(msg_int)
 
 
 if __name__ == "__main__":
-    encoder_type = EncoderEROB()
-    node = EncoderProcessNode(encoder=encoder_type)
+    node = EncoderProcessNode()
     rospy.spin()
---
+++ intelijet_v2_ws/src/encoder_process/src/encoder_process/base_encoder.py
@@ -0,0 +1,48 @@
+import struct
+import encoder_utils as solver
+
+class BaseEncoder:
+    """Interface chung cho tất cả encoder"""
+    def decode_angle(self, data: bytes) -> float:
+        """Decode giá trị góc từ CAN frame"""
+        raise NotImplementedError
+
+    def decode_raw(self, data: bytes) -> int:
+        """Decode giá trị raw từ CAN frame"""
+        raise NotImplementedError
+    
+
+class EncoderEROB(BaseEncoder):
+    """Encoder kiểu float32 + int32"""
+    def decode_angle(self, data: bytes) -> float:
+        return struct.unpack('<f', data[0:4])[0]
+
+    def decode_raw(self, data: bytes) -> int:
+        return struct.unpack('<i', data[4:8])[0]
+    
+
+class Encoder58x8(BaseEncoder):
+    """
+    Encoder EROB: decode CAN frame theo solver của bạn
+    - CAN data -> raw encoder value
+    - CAN data -> draw_wire_length -> angle (rad)
+    """
+    
+
+    def decode_angle(self, data: bytes) -> float:
+        """
+        Trả về góc (rad)
+        """
+        # convert bytes sang list of int nếu cần
+        message_data = list(data)
+        draw_wire_length = solver.convert_draw_wire_length(message_data)
+        angle = solver.length_to_angle_polynomial(draw_wire_length)
+        return angle
+
+    def decode_raw(self, data: bytes) -> int:
+        """
+        Trả về raw encoder value (int)
+        """
+        message_data = list(data)
+        raw_val, = solver.onvert_can_to_encoder_value(message_data)  # nếu hàm cũ trả 1 giá trị
+        return int(raw_val)
