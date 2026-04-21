0c2fd058b0d1fe30b90edb58a6013e6a29657286
Vo Hoai Danh <jacondv@gmail.com>
2026-04-21 15:03:30 +0700

    fix some small error in encoder code

--- intelijet_v2_ws/src/pps/scripts/dv_cloud_preprocess.py
+++ intelijet_v2_ws/src/pps/scripts/dv_cloud_preprocess.py
@@ -58,7 +58,7 @@ class CloudProcessorNode:
                                         min_bound=[cfg.crop_box.min.x, cfg.crop_box.min.y, cfg.crop_box.min.z], 
                                         max_bound=[cfg.crop_box.max.x, cfg.crop_box.max.y, cfg.crop_box.max.z])
         
-        cloud_o3d = remove_points_inside_box(pcd=cloud_o3d, min_point=(-0.5, -1, -0.5), max_point=(3, 1, 4))
+        cloud_o3d = remove_points_inside_box(cloud=cloud_o3d, min_point=(-0.5, -1, -0.5), max_point=(3, 1, 4))
         #Downsample
         
         cloud_o3d = cloudconverter.voxel_down_sample_spatial(cloud_o3d, voxel_size=0.015)
--- intelijet_v2_ws/src/pps/scripts/hmi_scan_command_handler.py
+++ intelijet_v2_ws/src/pps/scripts/hmi_scan_command_handler.py
@@ -8,8 +8,8 @@ from std_msgs.msg import String, Empty, Int32
 # from pps.msg import StartScanAction, StartScanGoal
 # from pps.msg import CompareCloudAction, CompareCloudGoal
 from pps.cloud_compare.compare_cloud_base_client import CompareBaseClient
-# from pps.sick_scan_controller import SickScanController
-from pps.sick_scan_eRob_controller import SickScanErobController
+from pps.sick_scan_controller import SickScanController
+# from pps.sick_scan_eRob_controller import SickScanErobController
 
 # from ros_blkarc_msgs.msg import TimedScanAction, TimedScanGoal
 
@@ -21,8 +21,8 @@ from shared.config_loader import CONFIG as cfg
 
 def get_scanner_controller(status_callback=None, active_lidar=cfg.active_lidar):
     if active_lidar == "lms511":
-        # controller = SickScanController(status_callback=status_callback)
-        controller = SickScanErobController(status_callback=status_callback)
+        controller = SickScanController(status_callback=status_callback)
+        # controller = SickScanErobController(status_callback=status_callback)
         return controller
     
 
